f8251e67a7a7a81236eb089a17e636fc752dd03a
narone <buzzin2426@gmail.com>
2025-08-02 19:29:09 +0900

    docs: 프로젝트 생성

---
+++ src/main/java/com/otbooalone/OtbooAloneApplication.java
@@ -0,0 +1,13 @@
+package com.otbooalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtbooAloneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OtbooAloneApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/otbooalone/OtbooAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otbooalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtbooAloneApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
