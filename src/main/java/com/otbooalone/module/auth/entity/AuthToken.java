73b90a54f260c669e9e484c1bf040ac320197b7d
narone <buzzin2426@gmail.com>
2025-08-12 19:40:50 +0900

    feat: 토큰 엔티티 수정

--- src/main/java/com/otbooalone/module/auth/entity/AuthToken.java
+++ src/main/java/com/otbooalone/module/auth/entity/AuthToken.java
@@ -13,6 +13,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -30,6 +31,10 @@ public class AuthToken{
   @CreatedDate
   @Column(columnDefinition = "timestamptz", updatable = false, nullable = false)
   private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(columnDefinition = "timestamptz")
+  private LocalDateTime updatedAt;
 
   @Column(nullable = false, unique = true)
   UUID userId;
