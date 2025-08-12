6564728eb12aeeef2937cce66334f9b9bcf78697
narone <buzzin2426@gmail.com>
2025-08-12 20:25:30 +0900

    chore: 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE "users"
 (
-    "id"                      UUID          PRIMARY KEY,
-    "created_at"              TIMESTAMPTZ   NOT NULL,
-    "updated_at"              TIMESTAMPTZ,
-    "email"                   VARCHAR(100)  NOT NULL,
-    "name"                    VARCHAR       NOT NULL,
-    "password"                VARCHAR       NOT NULL,
-    "gender"                  VARCHAR,
-    "birth_date"              DATE,
-    "temperature_sensitivity" INTEGER,
-    "profile_image_url"       TEXT,
-    "role"                    VARCHAR(10)   NOT NULL,
-    "locked"                  BOOLEAN       NOT NULL,
-    "location_id"             UUID
+    id                      UUID          PRIMARY KEY,
+    created_at              TIMESTAMPTZ   NOT NULL,
+    updated_at              TIMESTAMPTZ,
+    email                   VARCHAR(100)  NOT NULL,
+    name                    VARCHAR       NOT NULL,
+    password                VARCHAR       NOT NULL,
+    gender                  VARCHAR,
+    birth_date              DATE,
+    temperature_sensitivity INTEGER,
+    profile_image_url       TEXT,
+    role                    VARCHAR(10)   NOT NULL,
+    locked                  BOOLEAN       NOT NULL,
+    location_id             UUID
 );
 
 create table auth_tokens
