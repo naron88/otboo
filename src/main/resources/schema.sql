530d9eafc9ea4a05d655fb6d19398dca06252004
narone <buzzin2426@gmail.com>
2025-08-12 19:30:49 +0900

    chore: 스키마 및 yaml에 jwt 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "users"
+(
+    "id"                      UUID          PRIMARY KEY,
+    "created_at"              TIMESTAMPTZ   NOT NULL,
+    "updated_at"              TIMESTAMPTZ,
+    "email"                   VARCHAR(100)  NOT NULL,
+    "name"                    VARCHAR       NOT NULL,
+    "password"                VARCHAR       NOT NULL,
+    "gender"                  VARCHAR,
+    "birth_date"              DATE,
+    "temperature_sensitivity" INTEGER,
+    "profile_image_url"       TEXT,
+    "role"                    VARCHAR(10)   NOT NULL,
+    "locked"                  BOOLEAN       NOT NULL,
+    "location_id"             UUID
+);
+
+create table auth_tokens
+(
+    id            UUID          PRIMARY KEY,
+    user_id       UUID          NOT NULL UNIQUE,
+    access_token  TEXT,
+    refresh_token TEXT,
+    created_at    TIMESTAMPTZ   NOT NULL,
+    updated_at    TIMESTAMPTZ
+);
